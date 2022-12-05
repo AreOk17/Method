7616cb08e61b50fa9fb607b2e47699915c61a1c8
1 <Anasko162@Mail.ru>
2022-12-05 14:46:41 +0300

    Метод 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int x =10;
-        System.out.println(x);
+        int clientOS = 1;
+        int productionYear = 2015;
+        printInfoAboutDevice(clientOS, productionYear);
+
+    }
+public static String getOSbyType (int type) {
+        if (type == 1) {
+            return "Android";
+        } else {
+            return "iOS";
+
+        }
+}
+    private static String getTypeofVersion(int productionYear) {
+        int currentYear = LocalDate.now().getYear();
+        if (productionYear == currentYear) {
+            return "обычную ";
+        } else {
+            return "lite ";
+
+
+        }
+    }
+
+    private static void printInfoAboutDevice(int type, int productionYear) {
+        String os = getOSbyType(type);
+        String typeofVersion = getTypeofVersion(productionYear);
+        System.out.println("Установите " + typeofVersion + " версию приложения для " + os);
+
     }
 }
