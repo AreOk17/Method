7e0e669c4c1b27ec8cfd57805dea65cee7f0b3a3
1 <Anasko162@Mail.ru>
2022-12-05 14:13:22 +0300

    Первая задача по методу.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
 
-    int u = 10;
-        System.out.println("Hello world!");
+class Scratch {
+        public static void main(String[] args) {
+            int year = 2022;
+            boolean leapYear = isLeapYear(year);
+            printYearInfo(year, leapYear);
+        }
+        private static boolean isLeapYear(int year) {
+            return year % 4 == 0 && year % 100 != 0  || year % 400 == 0;
+
+        }
+        private  static void  printYearInfo(int year, boolean leapYear) {
+            if (leapYear) {
+                System.out.println("Год " + year + " високосный ");
+            }
+            else {
+                System.out.println("Год "+year+ " не високосный");
+
+            }
+        }
     }
-}
